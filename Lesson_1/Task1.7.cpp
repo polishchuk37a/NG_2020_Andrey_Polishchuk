5815dafb73420b7074d3416b577c697c96bd25ad
Andrey <Polishchuk.andrey2002@gmail.com>
2020-02-27 19:56:52 +0200

    Fixed identation task1.7

--- Lesson_1/Task1.7.cpp
+++ Lesson_1/Task1.7.cpp
@@ -7,16 +7,16 @@ int main()
     int a;
     cout << "Enter your salary: ";
     cin >> a;
+     if (a < 999){
+        cout << "Work more";
+    }
     if (a > 999999){
         cout << "You are millioner";
     }
-    if (a < 1000000){
-            if (a > 1000){
+    if (a > 1000){
+            if (a < 1000000){
         cout << "Okey";
             }
-    }
-    if (a < 999){
-        cout << "Work more";
     }
     cout << endl << "no ti molodec";
 }
