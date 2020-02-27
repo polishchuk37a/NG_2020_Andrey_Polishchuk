23127f551aee521435e9057c439d91a735c0e3f5
Andrey <Polishchuk.andrey2002@gmail.com>
2020-02-27 19:30:52 +0200

    Fixed the structure of Task1.6

--- Lesson_1/Task1.6.cpp
+++ Lesson_1/Task1.6.cpp
@@ -5,45 +5,31 @@ using namespace std;
 int main()
 {
     int a,b,c;
-    char d;
     cout << "Vvedite 1-e chislo: ";
     cin >> a;
     cout << "Vvedite 2-e chsilo: ";
     cin >> b;
     cout << "Vvedite 3-e chislo: ";
     cin >> c;
-    cout << "Vvedite deystviye *,+,-,/: ";
-    cin >> d;
-    switch (d)
-    {
-    case '*':
-        if (a*b==c or a*c==b or b*c==a)
-            {
-                cout << "Yes";
-                break;
-            }
-    case '+':
-        if (a+b==c or a+c==b or b+c==a)
-            {
-                cout << "Yes";
-                break;
-            }
-    case '-':
-        if (a-b==c or a-c==b or b-c==a)
-            {
-                cout << "Yes";
-                break;
-            }
-    case '/':
-        if (a/b==c or a/c==b or b/c==a)
-            {
-                cout << "Yes";
-                break;
-            }
+    if (a*b==c or a*c==b or b*c==a)
+        {
+            cout << "Yes";
+        }
+    else if (a+b==c or a+c==b or b+c==a)
+        {
+            cout << "Yes";
+        }
+    else if (a-b==c or a-c==b or b-c==a)
+        {
+            cout << "Yes";
+        }
+    else if (a/b==c or a/c==b or b/c==a)
+        {
+            cout << "Yes";
+        }
     else
         {
             cout << "No";
         }
     }
-}
 
