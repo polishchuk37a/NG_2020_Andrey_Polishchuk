ea27110f9c1d1e9d093e5f135f63680f091c5182
Andrey <Polishchuk.andrey2002@gmail.com>
2020-02-28 14:11:34 +0200

    Fixed algorytmin Task1.7

--- Lesson_1/Task1.7.cpp
+++ Lesson_1/Task1.7.cpp
@@ -7,16 +7,21 @@ int main()
     int a;
     cout << "Enter your salary: ";
     cin >> a;
-     if (a < 999){
-        cout << "Work more";
+    if(a < 1000)
+    {
+        cout << "You need to work more";
     }
-    if (a > 999999){
-        cout << "You are millioner";
+    if(a > 999)
+    {
+        if(a > 999999)
+        {
+          cout << "You are a millioner";
+        }
+        if(a < 1000000)
+        {
+          cout << "Okey";
+        }
     }
-    if (a > 1000){
-            if (a < 1000000){
-        cout << "Okey";
-            }
-    }
-    cout << endl << "no ti molodec";
+    cout << endl << "But you are a good boy :)";
+
 }
