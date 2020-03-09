e6bf9056859b3e1114b5357f6a3bf2fb2cf2f997
Andrey <Polishchuk.andrey2002@gmail.com>
2020-03-09 14:21:31 +0200

    The first task of the third lesson is ready

--- Lesson_3/Task3.1.cpp
+++ Lesson_3/Task3.1.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 int main()
 {
-    int schools[10];
-    for(int i = 0; i < schools[10]; i++){
-        cin >> i;
+    int school[10];
+    int your_school;
+    int else_changer = 0;
+    cout << "Enter numbers of schools." << endl;
+    for (int iterations = 0; iterations < 10; iterations++){
+        cout <<  iterations + 1 << ":";
+        cin >> school[iterations];
+    }
+    cout << "Enter the number of your school: ";
+    cin >> your_school;
+    for(int arraychek = 0; arraychek < 10; arraychek++){
+        if(your_school == school[arraychek]){
+            else_changer = 1;
+                cout << "I know this school!";
+            break;
+        }
+    }
+    if (else_changer == 0){
+        cout << "I don't know this school!";
     }
 }
