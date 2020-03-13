a83b47c351f07d4574e0b3f1290946a5290d848b
Andrey <Polishchuk.andrey2002@gmail.com>
2020-03-13 19:39:07 +0200

    Task3.2 is ready

---
+++ Lesson_3/Task3.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int cards[10],number,money;
+   cout << "Your balance on 10 cards" << endl;
+   for (int i = 0; i < 10; i++)
+   {
+       cards[i] = 0;
+       cout << cards[i] << " ";
+   }
+   cout << endl;
+   while(true)
+   {
+        cout << "Enter the number of card to put money: ";
+        cin >> number;
+        cout << "How much do you want to put? ";
+        cin >> money;
+        cards[number - 1] = cards[number - 1] + money;
+    for (int b = 0; b < 10; b++)
+    {
+        cout << cards[b] << " ";
+    }
+   cout << endl;
+  }
+}
